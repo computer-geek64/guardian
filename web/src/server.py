67fd873d997560328ce77752b3ac44c2f991cb3a
Ashish D'Souza <sudouser512@gmail.com>
2022-01-14 12:13:10 -0500

    Stream update

--- web/src/server.py
+++ web/src/server.py
@@ -4,7 +4,7 @@
 import os
 import cv2
 from auth import auth
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, stream_with_context
 from subprocess import Popen, PIPE, DEVNULL
 
 
@@ -21,28 +21,23 @@ def get_home():
 
 # Stream
 @app.route('/stream', methods=['GET'])
-def get_stream():
-    return render_template('cameras.html', title='Guardian', motion_username=os.environ['MOTION_USERNAME'], motion_password=os.environ['MOTION_PASSWORD']), 200
-
-
-@app.route('/image', methods=['GET'])
 def get_image():
-    camera = cv2.VideoCapture('rtsp://{username}:{password}@192.168.0.108:554/live/ch0'.format(username=os.environ['MOTION_USERNAME'], password=os.environ['MOTION_PASSWORD']))
+    camera = cv2.VideoCapture('rtsp://{username}:{password}@192.168.0.103:8000/live/ch0'.format(username=os.environ['MOTION_USERNAME'], password=os.environ['MOTION_PASSWORD']))
+    return Response(stream_with_context(generate_frames(camera)), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    def gen_frames():  # generate frame by frame from camera
+
+def generate_frames(camera):
+    try:
         while True:
-            # Capture frame-by-frame
-            success, frame = camera.read()  # read the camera frame
-            if not success:
-                break
-            else:
+            success, frame = camera.read()
+            if success:
                 ret, buffer = cv2.imencode('.jpg', frame)
                 frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
+                yield f'--frame\r\nContent-Type: image/jpeg\r\n\r\n{frame}\r\n'.encode()
+            else:
+                break
+    finally:
+        camera.release()
 
 
 if __name__ == '__main__':
