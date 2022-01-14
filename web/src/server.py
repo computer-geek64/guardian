3e0a5f7e9062b720da6faaa80021942d0763d358
Ashish D'Souza <sudouser512@gmail.com>
2022-01-13 19:32:51 -0500

    Fixed docker-compose OpenVPN conflict

--- web/src/server.py
+++ web/src/server.py
@@ -1,13 +1,13 @@
 #!/usr/local/bin/python
 # server.py
 
-import requests
+import os
 from auth import auth
-from flask import Flask, request, Response
+from flask import Flask, render_template
 from subprocess import Popen, PIPE, DEVNULL
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
 app.config['SESSION_TYPE'] = 'filesystem'
 app.secret_key = Popen(['uuidgen', '-r'], stdout=PIPE, stderr=DEVNULL).communicate()[0].decode().strip()
 
@@ -18,26 +18,10 @@ def get_home():
     return 'Welcome to Guardian!', 200
 
 
-@app.route('/1', methods=['GET'])
+# Stream
+@app.route('/stream', methods=['GET'])
 def get_stream():
-    return _proxy()
-
-
-def _proxy(*args, **kwargs):
-    resp = requests.request(
-        method=request.method,
-        url=request.url.replace(request.host_url, 'web'),
-        headers={key: value for (key, value) in request.headers if key != 'Host'},
-        data=request.get_data(),
-        cookies=request.cookies,
-        allow_redirects=False)
-
-    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-    headers = [(name, value) for (name, value) in resp.raw.headers.items()
-               if name.lower() not in excluded_headers]
-
-    response = Response(resp.content, resp.status_code, headers)
-    return response
+    return render_template('cameras.html', title='Guardian'), 200
 
 
 if __name__ == '__main__':
